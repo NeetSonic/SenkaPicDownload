85131d515d40afb98c1a91a5564006df864ea80e
Neetsonic <Neetsonic@gmail.com>
2017-10-18 16:50:22 +0800

    dev

--- SenkaPicDownload/Form1.Designer.cs
+++ SenkaPicDownload/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 19);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(65, 20);
+            label1.TabIndex = 2;
+            label1.Text = "起始时间";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(208, 19);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(65, 20);
+            label2.TabIndex = 4;
+            label2.Text = "结束时间";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(26, 61);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(51, 20);
+            label3.TabIndex = 5;
+            label3.Text = "镇守府";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(116, 98);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(157, 39);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "下载";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.BtnDownload_Click);
+            // 
+            // dateStart
+            // 
+            this.dateStart.CustomFormat = "yyyy/MM";
+            this.dateStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateStart.Location = new System.Drawing.Point(83, 16);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(96, 26);
+            this.dateStart.TabIndex = 1;
+            this.dateStart.Value = new System.DateTime(2013, 7, 1, 15, 47, 0, 0);
+            // 
+            // dateEnd
+            // 
+            this.dateEnd.Checked = false;
+            this.dateEnd.CustomFormat = "yyyy/MM";
+            this.dateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateEnd.Location = new System.Drawing.Point(279, 16);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(96, 26);
+            this.dateEnd.TabIndex = 3;
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Location = new System.Drawing.Point(83, 58);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(292, 28);
+            this.cmbServer.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(391, 149);
+            this.Controls.Add(this.cmbServer);
+            this.Controls.Add(label3);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.dateEnd);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.dateStart);
+            this.Controls.Add(this.btnDownload);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "战果人事表下载";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.DateTimePicker dateStart;
+        private System.Windows.Forms.DateTimePicker dateEnd;
+        private System.Windows.Forms.ComboBox cmbServer;
     }
 }
 
--- SenkaPicDownload/Form1.cs
+++ SenkaPicDownload/Form1.cs
@@ -1,12 +1,70 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Neetsonic.Tool;
 
 namespace SenkaPicDownload
 {
     public partial class Form1 : Form
     {
+        private static readonly DataTable Servers = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+            Servers.Columns.Add(@"Name", typeof(string));
+            Servers.Columns.Add(@"Value", typeof(int));
+            Servers.Rows.Add(@"横须贺", 1);
+            Servers.Rows.Add(@"吴", 2);
+            Servers.Rows.Add(@"佐世保", 3);
+            Servers.Rows.Add(@"舞鹤", 4);
+            Servers.Rows.Add(@"大凑", 5);
+            Servers.Rows.Add(@"特鲁克", 6);
+            Servers.Rows.Add(@"林加", 7);
+            Servers.Rows.Add(@"拉包尔", 8);
+            Servers.Rows.Add(@"肖特兰", 9);
+            Servers.Rows.Add(@"布因", 10);
+            Servers.Rows.Add(@"塔威塔威", 11);
+            Servers.Rows.Add(@"帕劳", 12);
+            Servers.Rows.Add(@"文莱", 13);
+            Servers.Rows.Add(@"单冠湾", 14);
+            Servers.Rows.Add(@"幌筵", 15);
+            Servers.Rows.Add(@"宿毛湾", 16);
+            Servers.Rows.Add(@"鹿屋", 17);
+            Servers.Rows.Add(@"岩川", 18);
+            Servers.Rows.Add(@"佐伯湾", 19);
+            Servers.Rows.Add(@"柱岛", 20);
+            cmbServer.DisplayMember = @"Name";
+            cmbServer.ValueMember = @"Value";
+            cmbServer.DataSource = Servers;
+        }
+
+        private void BtnDownload_Click(object sender, EventArgs e)
+        {
+            const string Url = "http://203.104.209.7/kcscontents/information/image/rank";
+            const int ServerCount = 20;
+            DateTime currDate = new DateTime(dateStart.Value.Year, dateStart.Value.Month, 1);
+            DateTime endDate = new DateTime(dateEnd.Value.Year, dateEnd.Value.Month, 1);
+            WebClient myWebClient = new WebClient();
+            while(currDate <= endDate)
+            {
+                int serverNO = Convert.ToInt32(cmbServer.SelectedValue);
+                //for(int serverNO = 1; serverNO <= ServerCount; serverNO++)
+                //{
+                    string filename = string.Format($@"{currDate.Year}{currDate.Month:00}{serverNO:00}.jpg").Substring(2);
+                    string url = string.Concat(Url, filename);
+                    string dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), @"战果人事表");
+                    if(!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+                    string filePath = Path.Combine(dir, filename);
+                    try { myWebClient.DownloadFile(url, filePath); }
+                    catch { }
+                //}
+                currDate = currDate.AddMonths(1);
+            }
+            MessageBoxEx.Info(@"完成！");
         }
     }
 }
